be90ff486ef5c6c07396fd8199ba38ff75f484a1
rgsk <guptarahul70322@gmail.com>
2024-07-16 17:59:16 +0530

    suspense wrap

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,12 @@
 import ChatPage from "@/components/ChatPage/ChatPage";
+import { Suspense } from "react";
 
 const Page = () => {
   return (
     <div>
-      <ChatPage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ChatPage />
+      </Suspense>
     </div>
   );
 };
